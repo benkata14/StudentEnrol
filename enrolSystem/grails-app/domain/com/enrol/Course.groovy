2d713dd14e3c65a7479c2e0e9eba945d756a0475
benkata14 <benkata2013@gmail.com>
2019-01-22 17:56:12 +0000

    Added Student and fixes to Course

---
+++ enrolSystem/grails-app/domain/com/enrol/Course.groovy
@@ -0,0 +1,26 @@
+package com.enrol
+
+class Course {
+
+String department
+String courseTitle
+String courseLeader
+String courseCode
+Date startDate
+Date endDate
+String description
+Integer numberOfStudents
+Double tuitionFees
+
+    static constraints = {
+courseTitle nullable: false, blank: false
+department nullable: false, blank: false
+courseLeader nullable: false, blank: false
+courseCode nullable: false, blank: false
+numberOfStudents nullable: false, blank: false, min: 20, max: 60
+startDate nullable: false, blank: false
+endDate nullable: false, blank: false
+description widget:'textarea',nullable: false, blank: false, max: 5000
+tuitionFees nullable: false, blank: false, scale: 2
+    }
+}
