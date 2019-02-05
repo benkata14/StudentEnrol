1c76d093b4c295989d289706b516f3d727416487
benkata14 <benkata2013@gmail.com>
2019-02-05 16:20:11 +0000

    added 2 more classes

--- enrolSystem/grails-app/domain/com/enrol/Course.groovy
+++ enrolSystem/grails-app/domain/com/enrol/Course.groovy
@@ -17,10 +17,10 @@ courseTitle nullable: false, blank: false
 department nullable: false, blank: false
 courseLeader nullable: false, blank: false
 courseCode nullable: false, blank: false
-numberOfStudents nullable: false, blank: false, min: 20, max: 60
+numberOfStudents nullable: false, blank: false, minSize: 20, maxSize: 60
 startDate nullable: false, blank: false
 endDate nullable: false, blank: false
-description widget:'textarea',nullable: false, blank: false, max: 5000
+description widget:'textarea',nullable: false, blank: false, maxSize: 5000
 tuitionFees nullable: false, blank: false, scale: 2
     }
 }
